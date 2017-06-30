477228ceee2c58f48437e205ca0ce217032ad3aa
Hans Georg Schaathun @kerckhoffs <georg@schaathun.net>
2017-06-30 15:14:16 +0200

    Bugfix in install script. Added test user for debugging use.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 docker run --rm  \
+       --link DB:DB \
        -v moodle-moodledata:/data \
-       moodle \
+       -v `pwd`/../moodle/:/var/www/html \
+       kqm/moodle \
        /etc/install.sh
